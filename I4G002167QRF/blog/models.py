72e273aedf1e4c84b27fcdad3747686df2bcbdf7
adnan ahmad <ahmadadnan2235@gmail.com>
2022-06-24 02:38:48 +0100

    update models.pu

--- I4G002167QRF/blog/models.py
+++ I4G002167QRF/blog/models.py
@@ -1,5 +1,17 @@
+
 from django.db import models
+from django.contrib.auth import get_user_model
+from django.utils import timezone 
+
+User = get_user_model()
 
 # Create your models here.
 class Post(models.Model):
     title = models.CharField(max_length=200)
+    text = models.TextField(max_length=255, default="")
+    author = models.ForeignKey(User, on_delete=models.CASCADE, default="")
+    created_date = models.DateTimeField(default="")
+    published_date = models.DateTimeField(default="")
+
+    def __str__(self):
+        return self.title
